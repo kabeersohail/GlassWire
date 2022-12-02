78fb0bd64156b4f7b0ce0ee9408858306e935359
KabeerSohail <sohail.mohammad@wenable.com>
2022-12-02 23:04:13 +0530

    Done
    1. Get data usage for single app per day

--- app/src/main/java/com/example/glasswire/MainActivity.kt
+++ app/src/main/java/com/example/glasswire/MainActivity.kt
@@ -3,20 +3,32 @@ package com.example.glasswire
 import android.app.AppOpsManager
 import android.app.AppOpsManager.MODE_ALLOWED
 import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
+import android.app.usage.NetworkStats
+import android.app.usage.NetworkStatsManager
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Process.myUid
+import android.os.RemoteException
 import android.provider.Settings.ACTION_USAGE_ACCESS_SETTINGS
 import android.telephony.TelephonyManager
+import android.util.Log
 import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.glasswire.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.ParseException
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.util.*
 
 /**
  * Article used to build this app
@@ -27,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    @RequiresApi(Build.VERSION_CODES.M)
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,26 +52,117 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            val networkStatsManager = this.getSystemService(NETWORK_STATS_SERVICE)
+            CoroutineScope(Dispatchers.IO).launch {
 
+                val start = LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
+                val end = ZonedDateTime.now().toInstant().toEpochMilli()
+
+                val mData = getAppWifiDataUsage(
+                    10459,
+                    start,
+                    end
+                )
+
+                val formattedData = formatData(mData[0], mData[1])
+
+                Log.d("SOHAIL BRO", "${formattedData.toList()}")
+            }
         }
-
     }
 
+    /**
+     * Formats the data
+     */
+    private fun formatData(sent: Long, received: Long): Array<String> {
+        val totalBytes = (sent + received) / 1024f
+        val sentBytes = sent / 1024f
+        val receivedBytes = received / 1024f
+
+        val totalMB = totalBytes / 1024f
+
+        val totalGB: Float
+        val sentGB: Float
+        val receivedGB: Float
+
+        val sentMB: Float = sentBytes / 1024f
+        val receivedMB: Float = receivedBytes / 1024f
+
+        var sentData = ""
+        var receivedData = ""
+        val totalData: String
+        if (totalMB > 1024) {
+            totalGB = totalMB / 1024f
+            totalData = String.format("%.2f", totalGB) + " GB"
+        } else {
+            totalData = String.format("%.2f", totalMB) + " MB"
+        }
+
+        if (sentMB > 1024) {
+            sentGB = sentMB / 1024f
+            sentData = String.format("%.2f", sentGB) + " GB"
+        } else {
+            sentData = String.format("%.2f", sentMB) + " MB"
+        }
+        if (receivedMB > 1024) {
+            receivedGB = receivedMB / 1024f
+            receivedData = String.format("%.2f", receivedGB) + " GB"
+        } else {
+            receivedData = String.format("%.2f", receivedMB) + " MB"
+        }
+
+        return arrayOf(sentData, receivedData, totalData)
+    }
+
+    @Throws(RemoteException::class, ParseException::class)
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun getAppWifiDataUsage(uid: Int, startTime: Long, endTime: Long): Array<Long> {
+
+        var sent = 0L
+        var received = 0L
+
+        val networkStatsManager: NetworkStatsManager =
+            applicationContext.getSystemService(NETWORK_STATS_SERVICE) as NetworkStatsManager
+        val networkStats: NetworkStats = networkStatsManager.querySummary(
+            ConnectivityManager.TYPE_WIFI,
+            getSubscriberID(),
+            startTime,
+            endTime
+        )
+
+        do {
+            val bucket = NetworkStats.Bucket()
+            networkStats.getNextBucket(bucket)
+            if (bucket.uid == uid) {
+                sent += bucket.txBytes
+                received += bucket.rxBytes
+            }
+        } while (networkStats.hasNextBucket())
+
+        val total: Long = sent + received
+        networkStats.close()
+
+        return arrayOf(sent, received, total)
+    }
 
     /**
      * Link to address deprecation
      *
      * 1. https://stackoverflow.com/questions/66896154/getinstallerpackagenamestring-string-is-deprecated-deprecated-in-java
      */
-    private fun uidOf(mPackage: String) = packageManager.getApplicationInfo("com.example.app", 0).uid
+    private fun uidOf(mPackage: String) = packageManager.getApplicationInfo(mPackage, 0).uid
 
     /**
      * Link describing the warning
      *
      * 1. https://stackoverflow.com/questions/47691310/why-is-using-getstring-to-get-device-identifiers-not-recommended
      */
-    private fun getSubscriberID(): String = (getSystemService(TELEPHONY_SERVICE) as TelephonyManager).subscriberId
+    private fun getSubscriberID(): String? = if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) try {
+        (getSystemService(TELEPHONY_SERVICE) as TelephonyManager).subscriberId
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    } else null
+
 
     /**
      * Displays a snack bar to request data usage permission
---
+++ app/src/main/java/com/example/glasswire/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.glasswire.utils
+
+object Constants {
+    const val RESET_HOUR = "resetHour"
+}
